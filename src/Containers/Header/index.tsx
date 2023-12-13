62e05834fc97b0ab57cc40341906c09b7aabc74f
Viktor Kryuchkov <crykour12@gmail.com>
2023-12-13 23:42:58 +0300

    fix Header

--- src/Containers/Header/index.tsx
+++ src/Containers/Header/index.tsx
@@ -3,12 +3,16 @@
 import { BtnType, Button } from 'Components';
 
 export const Header = () => (
-  <div className="py-3 px-6 rounded-lg bg-white">
-    <Button
-      onClick={() => {}}
-      type={BtnType.danger}
-    >
-      Logout
-    </Button>
+  <div className="flex justify-between items-center py-3 px-6 rounded-lg bg-white shadow-md">
+    <div className="font-bold text-2xl">JUZT</div>
+    <div className="flex items-center">
+      <div className="mr-3">aaa@gmail.com</div>
+      <Button
+        onClick={() => {}}
+        type={BtnType.danger}
+      >
+        Logout
+      </Button>
+    </div>
   </div>
 );
